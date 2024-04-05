8547b3b5875df427aee0e5ba031772568b38cd70
Iscle <albertiscle9@gmail.com>
2024-04-05 02:24:36 +0200

    android: Improve MainActivity

--- android/app/src/main/java/me/iscle/doh/MainActivity.kt
+++ android/app/src/main/java/me/iscle/doh/MainActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import me.iscle.doh.ui.theme.DohTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,19 +21,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DohTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val context = LocalContext.current
-                   Button(onClick = {
-                       Intent(context, GameActivity::class.java).also {
-                           startActivity(it)
-                       }
-                   }) {
-                       Text("Start game")
-                   }
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding).fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        val context = LocalContext.current
+                        Button(onClick = {
+                            Intent(context, GameActivity::class.java).also {
+                                startActivity(it)
+                            }
+                        }) {
+                            Text("Start game")
+                        }
+                    }
                 }
             }
         }
